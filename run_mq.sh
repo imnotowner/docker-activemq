b382df28755dc5887b4e571b9442280260aa2e33
blacknoir <blacknoir@blacknoir>
2015-03-25 16:02:24 +0500

    Change classpath and confpath

--- run_mq.sh
+++ run_mq.sh
@@ -5,10 +5,10 @@ cd /opt/activemq
 java -Xms256M -Xmx256M \
 	-Djava.util.logging.config.file=logging.properties \
 	-Dcom.sun.management.jmxremote \
-	-Dactivemq.classpath=/data/broker \
+	-Dactivemq.classpath=/data/broker/conf \
 	-Dactivemq.home=/opt/activemq \
 	-Dactivemq.base=/opt/activemq \
-	-Dactivemq.conf=/data/broker \
+	-Dactivemq.conf=/data/broker/conf \
 	-Dactivemq.data=/data/broker \
 	-jar /opt/activemq/bin/activemq.jar \
 	start
